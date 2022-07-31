f4e74fa04567b964876130ef0f1c48df1644f726
Dylan Gardner <dylangardner157@gmail.com>
2022-07-31 17:45:36 +0200

    Adittional capacitor changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'word-app',
-  webDir: './src',
+  webDir: 'dist/word-app',
   bundledWebRuntime: false,
 };
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:3000',
+  client_id: 'c3c3c151679142ac3edc95eef4cddb5919e2145369bc09783f8ca1b2ded3745ff33f05556e669e7030473a61917626035aa6a223b3565c1901ad424954766c8c',
+  client_secret: '3177aa7732c46d9d48c253f8dd94153f4fa7f4aaf938b061f5880d7f67d0756dc6e7beaedc11058f72d7d89a8d5542c2a1704fcb911ce600ce2763eab5b5d290'
 };
